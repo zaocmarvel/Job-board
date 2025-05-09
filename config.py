f05957c80292144fb03e7e33e8b0bcb84b014100
zaocmarvel <zacchaeusoluwasayo@gmail.com>
2025-05-09 10:20:20 +0100

    Add files via upload

---
+++ config.py
@@ -0,0 +1,9 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+class Config:
+    SECRET_KEY = os.getenv('SECRET_KEY', 'dev-secret-key')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'sqlite:///site.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
